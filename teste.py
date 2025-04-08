4ba45884a7e4cd208284d5ead547293537adc448
Malujoro <mateus_rocha_sousa@hotmail.com>
2025-04-07 22:32:28 -0300

    Implementação do CycleSort

--- teste.py
+++ teste.py
@@ -6,41 +6,44 @@ tam = 10
 crescente = [i for i in range(tam)]
 decrescente = [i for i in range(tam-1, -1, -1)]
 aleatorio = [random.randint(0, tam) for i in range(tam)]
-aleatorio2 = aleatorio[:]
 
 print(f"Crescente: {crescente}")
 print(f"Decrescente: {decrescente}")
-print(f"Aleatório: {aleatorio} {aleatorio2}")
+print(f"Aleatório: {aleatorio}")
 
 def heapSort(vetor: list[int], tam: int):
-    quantidade = tam-1
-    while(quantidade >= 0):
+    for quantidade in range(tam-1, -1, -1):
         for filho in range(quantidade, 0, -1):
             pai = (filho - 1) // 2
             if(vetor[filho] > vetor[pai]):
                 vetor[filho], vetor[pai] = vetor[pai], vetor[filho]
 
         vetor[quantidade], vetor[0] = vetor[0], vetor[quantidade]
-        quantidade -= 1
+
+def cycleSort(vetor: list[int], tam: int):
+    for it in range(tam-1):
+        aux = vetor[it]
+        while(True):
+            quantMenor = 0
+
+            for item in vetor[it+1:]:
+                if(item < aux):
+                    quantMenor += 1
+            
+            if(quantMenor == 0):
+                vetor[it] = aux
+                break
+            
+            posicao = quantMenor + it
+            while(vetor[posicao] == aux):
+                posicao += 1
+
+            aux, vetor[posicao] = vetor[posicao], aux
 
 
-for vetor in [aleatorio, crescente, decrescente]:
-    print(vetor)
-    heapSort(vetor, tam)
-    print(vetor)
-    print()
-
-# Fórmula para encontrar posição do filho:
-# I * 2 + 1 (esquerda)
-# I * 2 + 2 (direita)
-
-# Fórmula para encontrar posição do pai:
-# (Posição - 1) // 2
-
-#  0, 1, 2,  3, 4, 5, 6, 7, 8, 9
-# [8, 1, 10, 7, 2, 8, 0, 5, 6, 9]
-# 0 - 1 e 2
-# 1 - 3 e 4
-# 2 - 5 e 6
-# 3 - 7 e 8
-# 4 - 9 e 10
+for funcao in [heapSort, cycleSort]:
+    for vetor in [aleatorio[:], crescente[:], decrescente[:]]:
+        print(vetor)
+        funcao(vetor, tam)
+        print(vetor)
+        print()
